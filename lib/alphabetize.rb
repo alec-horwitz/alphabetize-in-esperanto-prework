c08dd058620ac65e26997cc4a62d4b18e09e62bc
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 23:33:55 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,3 @@
 def alphabetize(arr)
-  arr.sort_by {|string| string.split("").map{ |a| "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".index(a)}}
+  arr.sort_by {|string| string.split("").map{ |character| "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".index(character)}}
 end
