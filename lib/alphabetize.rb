2b0343e2aa9960555aac585a20b7385e77d6100e
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 23:30:26 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ require "pry"
 def alphabetize(arr)
   # code here
   espAlp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|m| m.split("").map{ |a, b| espAlp.index(a) <=> espAlp.index(b) }} #{ |a, b| espAlp.index(a) <=> espAlp.index(b) }
+  arr.sort_by {|m| m.split("").map{ |a| espAlp.index(a)}} #{ |a, b| espAlp.index(a) <=> espAlp.index(b) }
 
 end
